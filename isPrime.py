c6037d4d2ca0b9ff085f230bda468b868a0151d1
Vivek Nagose <nagosevivek19@gmail.com>
2022-09-05 20:14:48 +0530

    Check whether number is prime or not

---
+++ isPrime.py
@@ -0,0 +1,12 @@
+def prime(n):
+    for i in range(2, n+1):
+        if (n%i) == 0:
+            break
+    if i==n:
+        print('prime')
+    else:
+        print('not prime')
+
+n=int(input())
+prime(n)
+              
